921730cd341a24a4e573700e1f6ff2b2b56d731b
Gary J <github@accu.ca>
2022-01-04 11:27:40 -0800

    Add Form

--- Week-3/lecture-1/react-exercise/README.md
+++ Week-3/lecture-1/react-exercise/README.md
@@ -1,10 +1,5 @@
-# Introduction to State
+# React Form Exercise
 
-- [ ] Rendering of React Functional Components
-- [ ] What is State?
-- [ ] Why do we need to consider State
-- [ ] How React manages State:  the `useState` "hook"
-- [ ] Rules of Hooks
-- [ ] Examples of State
-- [ ] Programming Exercise: Independent work (45 min)
-- [ ] Programming Exercise: Review (15 min)
+- [ ] Html goes in the `return`
+- [ ] Add event handlers
+- [ ] Render changed state or data in the `return`
--- Week-3/lecture-1/react-exercise/src/App.js
+++ Week-3/lecture-1/react-exercise/src/App.js
@@ -1,10 +1,26 @@
-import 'App.css';
+import "App.css";
+import { useState } from "react";
 
 export default function App() {
 
+  
   return (
     <div className="App">
-      <h1>Hello React</h1>
+
+      <h1> Guess the number </h1>
+
+      <form >
+        <label class="guess">
+          Write a number between 1 and 10:
+          <br />
+          <input />
+        </label>
+        <br />
+        <button> Guess </button>
+      </form>
+
+      <button> Reset </button>
+      
     </div>
   );
 }
