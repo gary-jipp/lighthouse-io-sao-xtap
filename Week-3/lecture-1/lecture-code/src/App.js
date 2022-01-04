a411a70728cd00378764ba1c443361465e5ef872
Gary J <github@accu.ca>
2022-01-04 10:56:28 -0800

    Add Form

--- Week-3/lecture-1/lecture-code/src/App.js
+++ Week-3/lecture-1/lecture-code/src/App.js
@@ -1,10 +1,37 @@
 import 'App.css';
+import { useState } from 'react';
 
 export default function App() {
+  // console.log("App Component Rendering");
+  const [text, setText] = useState("");
+  const [otherText, setOtherText] = useState("");
+
+  const onChange = function(event) {
+    setText(event.target.value);
+  };
+
+  const onClick = function() {
+    setOtherText("Input: " + text);
+  };
+
+  const onSubmit = function(event) {
+    event.preventDefault();
+    console.log("onSubmit Called");
+    setOtherText(<h2>{`${text}`}</h2>);
+  };
 
   return (
     <div className="App">
       <h1>Hello React</h1>
+
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Submit</button>
+      </form>
+
+      <div>
+        {otherText}
+      </div>
     </div>
   );
 }
