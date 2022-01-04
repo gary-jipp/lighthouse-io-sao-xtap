302eb49335b941cf134de5795fe4b4dba0bed32e
Gary J <github@accu.ca>
2022-01-04 12:12:07 -0800

    Update

--- Week-3/lecture-1/react-exercise/src/App.js
+++ Week-3/lecture-1/react-exercise/src/App.js
@@ -2,25 +2,57 @@ import "App.css";
 import { useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [guess, setGuess] = useState(-1);
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 10) + 1);
 
-  
+  const onChange = function(event) {
+    setInput(event.target.value);
+  };
+
+  const onSubmit = function(event) {
+    event.preventDefault();
+    const number = Number(input);
+    if (!isNaN(number)) {
+      setGuess(number);
+    }
+
+  };
+
+
+  let result = "You Got it!";
+  if (guess < randomNum) {
+    result = "That's too low!";
+  }
+  if (guess > randomNum) {
+    result = "That's too high!";
+  }
+
+  const reset = function() {
+    setRandomNum(Math.floor(Math.random() * 10) + 1);
+    setInput("");
+    setGuess(-1);
+  };
+
   return (
     <div className="App">
 
       <h1> Guess the number </h1>
 
-      <form >
+      <form onSubmit={onSubmit}>
         <label class="guess">
           Write a number between 1 and 10:
           <br />
-          <input />
+          <input value={input} onChange={onChange} />
         </label>
         <br />
         <button> Guess </button>
       </form>
 
-      <button> Reset </button>
-      
+      <button onClick={reset}> Reset </button>
+      {(guess > 0) && <h5>
+        {result}
+      </h5>}
     </div>
   );
 }
