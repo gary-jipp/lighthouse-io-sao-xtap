fbde39a8799001b4345564854cbeb4faee3e974a
Gary J <github@accu.ca>
2022-01-06 12:16:40 -0800

    Add axios & list

--- Week-3/lecture-2/react-exercise/src/App.js
+++ Week-3/lecture-2/react-exercise/src/App.js
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'App.css';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://random-data-api.com/api/name/random_name?size=10")
+      .then(res => setData(res.data));
+  }, [num]);
+
+  const list = data.map(item => {
+    return (
+      <li>
+        <p>
+          ID: {item.id}
+        </p>
+        <p>
+          Name:  {item.name}
+        </p>
+      </li>
+    );
+  });
+
+  const onClick = function() {
+    setNum(num + 1);
+  };
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Users</h1>
+      <button onClick={onClick}>Refresh</button>
+      <ul>
+        {list}
+      </ul>
     </div>
   );
 }
