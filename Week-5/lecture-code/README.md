03b006a34db9fef12dd2dd8889844a54126826d3
Gary J <github@accu.ca>
2022-01-18 08:47:53 -0800

    Update

--- Week-5/lecture-code/README.md
+++ Week-5/lecture-code/README.md
@@ -1,7 +1,1 @@
-# React useReducer
-
-[ ] What is useReducer?
-[ ] Why not just useState?
-
-
-The useReducer() hook lets you separate the state management from the rendering logic of the component.
+# Minimal React App v5
--- Week-5/lecture-code/src/App.js
+++ Week-5/lecture-code/src/App.js
@@ -1,87 +1,14 @@
-import "App.css";
-import Counter from "components/Counter";
-import Counter1 from "components/Counter1";
-import { useState, useReducer } from "react";
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-const CLEAR = "CLEAR";
+import './App.css';
 
-const countReducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      break;
-    case CLEAR:
-      return 0;
-    default:
-      return state;
-  }
-};
-
-export default function App() {
-  const [count1, setCount1] = useState(0);
-  const [count2, dispatch2] = useReducer(countReducer, 0);
-  const [count3, setCount3] = useState(0);
-
-  const [count, dispatch] = useReducer(countReducer, 0);
-
-  const increment1 = function() {
-    setCount1(count1 + 1);
-  };
-  const decrement1 = function() {
-    setCount1(count1 - 1);
-  };
-  const clear1 = function() {
-    setCount1(0);
-  };
-
-  const increment2 = function() {
-    dispatch2({ type: INCREMENT });
-  };
-  const decrement2 = function() {
-    dispatch2({ type: DECREMENT });
-  };
-  const clear2 = function() {
-    dispatch2({ type: CLEAR });
-  };
-
-  const increment3 = function() {
-    setCount3(count3 + 1);
-  };
-  const decrement3 = function() {
-    setCount3(count3 - 1);
-  };
-  const clear3 = function() {
-    setCount3(0);
-  };
+function App() {
 
   return (
-    <div class="App">
-      <h1>Counters!</h1>
-      <div class="counter">
-        <span>{count1}</span>
-        <button onClick={decrement1}>-</button>
-        <button onClick={clear1}>0</button>
-        <button onClick={increment1}>+</button>
-      </div>
-
-      <div class="counter">
-        <span>{count2}</span>
-        <button onClick={decrement2}>-</button>
-        <button onClick={clear2}>0</button>
-        <button onClick={increment2}>+</button>
-      </div>
-
-      <Counter1
-        count={count3}
-        increment={increment3}
-        decrement={decrement3}
-        clear={clear3} />
-
-
-      <Counter count={count} dispatch={dispatch} />
-
+    <div className="App">
+      <h2>
+        Hello React
+      </h2>
     </div>
   );
 }
+
+export default App;
