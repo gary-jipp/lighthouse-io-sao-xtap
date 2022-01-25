6b76437673efd9f2ae6a5db29d068e4215200340
Gary J <github@accu.ca>
2022-01-24 21:23:20 -0800

    Update

--- Week-6/lecture-1/lecture-code/src/App.js
+++ Week-6/lecture-1/lecture-code/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, } from 'react';
+import { useState, useCallback, } from 'react';
 import Header from 'components/Header';
 import uniqid from 'uniqid';
 import axios from 'axios';
